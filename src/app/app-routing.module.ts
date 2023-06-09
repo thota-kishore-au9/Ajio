e93e96dc3ca2836dfd2ebec5d731daa19524f6b8
THOTA kishore <thotakishore90@gmail.com>
2023-06-09 23:27:58 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'homepage',component:HomepageComponent},
+  {path:'navbar',component:NavbarComponent},
+  {path:'footer',component:FooterComponent}
+
+
+
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
